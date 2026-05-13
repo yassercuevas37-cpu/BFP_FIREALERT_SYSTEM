3fbdd9a21fd3b0e666efefb33044581b523234a1
yassercuevas37-cpu <yassercuevas37@gmail.com>
2026-05-13 22:31:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,14 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
+
+  preview: {
+    allowedHosts: true
+  }
 });
